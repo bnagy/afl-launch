8945b8c435138cd8372eb9ad1099908db4868245
ben <git@ben.iagu.net>
2015-06-09 09:15:23 +0700

    change -t to string to support -t 100+ syntax

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var (
 	flagNoMaster = flag.Bool("no-master", false, "Launch all instances with -S")
 	flagNum      = flag.Int("n", 1, "Number of instances to launch")
 	flagName     = flag.String("name", "", "Base name for instances. Fuzzers will work in <output>/<BASE>-[M|S]<N>")
-	flagTimeout  = flag.Int("t", -1, "afl-fuzz -t option (timeout)")
+	flagTimeout  = flag.String("t", "", "afl-fuzz -t option (timeout)")
 	flagMem      = flag.Int("m", -1, "afl-fuzz -m option (memory limit)")
 	flagInput    = flag.String("i", "", "afl-fuzz -i option (input location)")
 	flagExtras   = flag.String("x", "", "afl-fuzz -x option (extras location)")
